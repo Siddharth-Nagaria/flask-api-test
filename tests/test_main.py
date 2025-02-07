6e961d4d2453058dc6f600f60b46f424710e4f6b
Siddharth Nagaria/Big Data <siddharth.nagaria@piramal.com>
2025-02-07 16:50:18 +0530

    print response01

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,7 +12,7 @@ def test_landing_unit_test():
     response = app.test_client().get('/')
     assert response.status_code == 200
     assert b'Hi, Welcome to Unit testing' in response.data
-    logger.info(f"received response {response.data}")
+    logger.info(f"received response {response}")
 
 def test_login_unit_test():
     response = app.test_client().get('/login/sid')
