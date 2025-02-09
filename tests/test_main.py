f189878aee6ccf725fd5aca417dd82a3cc80fefc
Siddharth Nagaria/Big Data <siddharth.nagaria@piramal.com>
2025-02-10 03:25:42 +0530

    log_check02

--- tests/test_main.py
+++ tests/test_main.py
@@ -19,6 +19,8 @@ def test_login_unit_test():
     response = app.test_client().get('/login/sid')
     assert response.status_code == 200
     assert b'sid testing' in response.data
+    logger.info(f"Response data: {response.data.decode()}")
+
 
 def test_factorial_unit_test():
     response = app.test_client().get('/factorial/7')
@@ -29,35 +31,43 @@ def test_factorial_unit_test():
     assert json_data["number"]==7 
     assert "factorial" in json_data
     assert json_data["factorial"]==5040  
+    logger.info(f"Response data: {json_data}")
+
 
 
 def test_handle_get_valid_credentials():
     response = app.test_client().get('/handle_get?username=siddharth&password=1234')
     assert response.status_code == 200
     assert b'Welcome!!!' in response.data
+    logger.info(f"Response data: {response.data.decode()}")
 
 def test_handle_get_invalid_credentials():
     response = app.test_client().get('/handle_get?username=siddharth&password=wrongpassword')
     assert response.status_code == 200
     assert b'invalid credentials!' in response.data
+    logger.info(f"Response data: {response.data.decode()}")
 
 def test_handle_get_missing_credentials():
     response = app.test_client().get('/handle_get?username=&password=')
     assert response.status_code == 200
     assert b'invalid credentials!' in response.data
+    logger.info(f"Response data: {response.data.decode()}")
 
 def test_handle_post_valid_credentials():
     response = app.test_client().post('handle_post', data = {'username': 'siddharth', 'password': '1234'})
     assert response.status_code == 200
     assert b'Welcome!!!' in response.data
+    logger.info(f"Response data: {response.data.decode()}")
 
 def test_handle_post_invalid_credentials():
     response = app.test_client().post('handle_post', data = {'username': 'siddharth', 'password': 'wrong_password'})
     assert response.status_code == 200
     assert b'invalid credentials!' in response.data
+    logger.info(f"Response data: {response.data.decode()}")
 
 def test_handle_post_missing_credentials():
     response = app.test_client().post('handle_post', data = {'username': '', 'password': ''})
     assert response.status_code == 200
     assert b'invalid credentials!' in response.data
+    logger.info(f"Response data: {response.data.decode()}")
 
