0998c38f5f4ec357ee6bd24ab021506d8630d340
Siddharth Nagaria/Big Data <siddharth.nagaria@piramal.com>
2025-02-20 10:58:01 +0530

    json name modified

--- dynamic.py
+++ dynamic.py
@@ -55,7 +55,7 @@ for item in paths:
     }
 
 # Output the YAML to a file
-yaml_file_path = f"api_gateway_config.yaml"
+yaml_file_path = f"api-gateway-config.yaml"
 with open(yaml_file_path, 'w') as yaml_file:
     yaml.dump(yaml_data, yaml_file, default_flow_style=False)
 
